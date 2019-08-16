ca2032ba1b04f6a23d8c437f9e5392910bfdc3d2
autumn-ragland <raglanda998@gmail.com>
2019-08-16 14:15:24 -0500

    Graded 08-16-2019 14:13:44

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ alert ("Test");
 // The book [BOOK NAME] has [RATING] stars!!!
 // ```
 
-;
-
 class Books
-{constructor(name, rating, genre, author)
+{
+    constructor(name, rating, genre, author)
     {
         this.name = name;
         this.rating = rating;
@@ -19,20 +18,18 @@ class Books
     }
 }
 
-function main(
-    {
-
+function main(){ // PLEASE check you code bloack. This DID NOT run at all just bc of a syntax error
     var name =[];
+    // you did not create 3 instances of the class
     // let rating=[1,2,3];
 
-name.forEach(
-    function(eachElement)
-    {
-        name.push("the awakening","assata","go go");
-    }
-)
-console.log(name);
-);
-}
+    name.forEach( // a forEach function will only fun the function for each item in your array. You array is empty so the function will not run at all. You could have manually pushed each object into the arary
+        function(eachElement)
+        {
+            name.push("the awakening","assata","go go"); // you are supposed to push 3 OBJECTS (instances of a class) into the array
+        }
+    )
+    console.log(name);
+};
 
 main();
